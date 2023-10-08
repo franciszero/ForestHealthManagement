17220d46870d55ab2fdf45e5ae085713cbc0898c
Francis Chan <franciszero@gmail.com>
2023-10-07 20:24:46 -0400

    .

--- src/1.py
+++ src/1.py
@@ -1,82 +1,126 @@
-import rasterio as rio  # the GEOS-based raster package
 from pyhdf.SD import SD, SDC
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import re
 from datetime import datetime, timedelta
+import pandas as pd
+import seaborn as sns
 
 
 class Foo:
     def __init__(self):
         self.rootpath = './h12v04'
-        self.hdf_files = [f for f in os.listdir(self.rootpath) if f.endswith('.hdf')]
-        self.ndvi_data = []
+        self.hdf_path = f'{self.rootpath}/data'
+        self.hdf_files = sorted([f for f in os.listdir(self.hdf_path) if f.endswith('.hdf')],
+                                key=self.__extract_date_from_filename)
+
+        # # Check if saved data exists
+        # self.__is_data_saved = False
+        # if os.path.exists(os.path.join(self.rootpath, 'ndvi.npy')) and \
+        #         os.path.exists(os.path.join(self.rootpath, 'evi.npy')) and \
+        #         os.path.exists(os.path.join(self.rootpath, 'dates.npy')):
+        #     self.__data_saved = True
+        #     self.load_data()
+        # else:
+        self.ndvi = []
+        self.evi = []
         self.dates = []
+        self.__foo()
         pass
 
     @staticmethod
-    def extract_date_from_filename(filename):
-        year = int(filename[9:13])
-        doy = int(filename[13:16])  # day of year
+    def __extract_date_from_filename(fn):
+        year = int(fn[9:13])
+        doy = int(fn[13:16])  # day of year
         date = datetime(year, 1, 1) + timedelta(doy - 1)
         return date.strftime('%Y-%m-%d')
 
-    def foo(self):
+    def __foo(self):
+        plt.figure(figsize=(10, 10))
         for hdf_file in self.hdf_files:
-            dt = self.extract_date_from_filename(hdf_file)
+            dt = self.__extract_date_from_filename(hdf_file)
             print(dt)
             self.dates.append(dt)
 
-            file_path = os.path.join(self.rootpath, hdf_file)
-
-            hdf = SD(file_path, SDC.READ)
+            hdf = SD(os.path.join(self.hdf_path, hdf_file), SDC.READ)
             for idx, sds in enumerate(hdf.datasets().keys()):
+                print("\t%-40s" % sds, end=" ")
+                ds = hdf.select(sds)
+                arr = ds[:]
                 if "NDVI" in sds:
-                    print(hdf_file, sds)
-                    ds = hdf.select(sds)
-                    self.ndvi_data.append(ds[:])
+                    self.ndvi.append(arr)
+                if "EVI" in sds:
+                    self.evi.append(arr)
 
-                    # self.plot_map(arr, sds, hdf_file)
+                sds = sds.replace(' ', '_')
+                pth = f"{self.rootpath}/{sds}"
+                if not os.path.exists(pth):
+                    os.makedirs(pth)
+                fp = f"{pth}/{dt}.png"
+                if not os.path.exists(fp):
+                    print(f"plotting map {fp}", end=" ")
+                    self.__plot_map(arr, sds, fp)
+                else:
+                    print(f"skipping plot {fp}", end=" ")
+                print()
+
             hdf.end()
+        # self.save_data()
 
-    def plot_map(self, arr, sds, hdf_file):
-        plt.figure(figsize=(10, 10))
+    @staticmethod
+    def __plot_map(arr, sds, fp):
         plt.imshow(arr, cmap='RdYlGn')
         plt.colorbar(label=sds)
         plt.title(sds)
-        dst_file = sds.replace(" ", "_") + ".png"
-        dst_folder = self.rootpath + "/" + hdf_file.rsplit(".", 1)[0] + "/"
-        if not os.path.exists(dst_folder):
-            os.makedirs(dst_folder)
-        plt.savefig(dst_folder + dst_file)
+        plt.savefig(fp)
         plt.clf()
 
     def plot_time_series(self, x, y):
-        ts = np.array(foo.ndvi_data)[:, x, y]
-        plt.figure(figsize=(12, 6))
-        plt.plot(self.dates, ts, marker='o', linestyle='-', color='b')
-        plt.title(f"Time Series for Pixel ({x}, {y})")
-        plt.xlabel("Date")
-        plt.ylabel("Average NDVI")
-        plt.grid(True)
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        # plt.show()
-        plt.savefig(os.path.join(self.rootpath, "NDVI_time_series.png"))
+        pth = f"{self.rootpath}/time_series"
+        if not os.path.exists(pth):
+            os.makedirs(pth)
 
-    @staticmethod
-    def _extract_date_from_filename(filename):
-        """Extract date information from the filename."""
-        match = re.search(r'A(\d{4})(\d{3})', filename)
-        if match:
-            year = match.group(1)
-            day_of_year = match.group(2)
-            return f"{year}-{day_of_year}"
-        return "Unknown-Date"
+        df = pd.DataFrame(index=range(len(self.dates)), columns=["dt", "ndvi", "evi"])
+        df["dt"] = self.dates
+        df["ndvi"] = np.array(self.ndvi)[:, x, y]
+        df["evi"] = np.array(self.evi)[:, x, y]
+        sns.lineplot(data=df)
+        fp = f"%s/%4d_%4d.png" % (pth, x, y)
+        plt.savefig(fp)
+        print(f"plot saved at {fp}")
+        plt.clf()
+
+        # plt.plot(self.dates, ts1, marker='o', linestyle='-', color='b')
+        # plt.plot(self.dates, ts2, marker='o', linestyle='-', color='b')
+        # plt.title(f"Time Series for Pixel ({x}, {y})")
+        # plt.xlabel("Date")
+        # plt.ylabel("Vegetation Indices")
+        # plt.grid(True)
+        # plt.xticks(rotation=45)
+        # plt.tight_layout()
+        # plt.show()
+        # fp = f"%s/%4d_%4d.png" % (pth, x, y)
+        # if not os.path.exists(fp):
+        #     plt.savefig(fp)
+        # plt.clf()
+
+    def save_data(self):
+        np.save(os.path.join(self.rootpath, 'ndvi.npy'), self.ndvi)
+        # np.save(os.path.join(self.rootpath, 'evi.npy'), self.evi)
+        np.save(os.path.join(self.rootpath, 'dates.npy'), self.dates)
+
+    def load_data(self):
+        self.ndvi = np.load(os.path.join(self.rootpath, 'ndvi.npy'), allow_pickle=True).tolist()
+        self.evi = np.load(os.path.join(self.rootpath, 'evi.npy'), allow_pickle=True).tolist()
+        self.dates = np.load(os.path.join(self.rootpath, 'dates.npy'), allow_pickle=True).tolist()
 
 
 if __name__ == "__main__":
     foo = Foo()
-    foo.foo()
-    foo.plot_time_series(0, 0)
+
+    plt.figure(figsize=(12, 6))
+    for x in range(0, 5000, 1000):
+        for y in range(0, 5000, 1000):
+            foo.plot_time_series(x, y)
+
+    i = 0
